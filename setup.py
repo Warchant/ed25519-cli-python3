41598a33c2b92debfe1df173bcfa418a6e075e22
Bogdan Vaneev <warchantua@gmail.com>
2018-09-27 09:21:32 +0300

    Remove package pysha3
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyed25519-sha3',
     version='0.0.1',
-    packages=['pysha3'],
+    packages=[''],
     url='https://github.com/Warchant/pyed25519-sha3',
     license='Apache 2.0',
     author='warchant',
